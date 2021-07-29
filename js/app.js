8163df5a4fc1e2000344e25c3c24cb4e53932d84
cesar londoño <slac_@hotmail.com>
2021-07-29 12:24:19 -0400

    0002

--- js/app.js
+++ js/app.js
@@ -1,24 +1,66 @@
 console.log('hola');
-
+/*
 const productosEntrar= $('#productos');
 const cantidadProductos = $('#cantidadEntrada');
 const productoVentas = $('#productosVentas');
 const cantidadVenta = $('#cantidadVenta');
+*/
+const btnIngresa = $('#agregar');
+// const btnVender = $('#vender');
 
-const btnIngresa = $('#ingresar');
-const btnVender = $('#vender');
+const ingresoProduto = $('#ingresoProduto');
 
 
+const stock = [
 
-console.log(ingresar);
-const stockEntrada = [];
+    {
+      producto: 'Amonio C',
+      id : '01',
+      Stock : '1502',
+      Precio : 3750
+    },
 
-btnIngresa.click(function () {
-    crearStock();
-    console.log(stockEntrada);
-    console.log(productosEntrar.val());
+    {
+      producto: 'Cloro Gel',
+      id : '02',
+      Stock : '1025',
+      Precio : 1350 
+    },
+
+    {
+      producto: 'Lisoform',
+      id : '03',
+      Stock : '327',
+      Precio : 2590
+    }
+
+];
+
+btnIngresa.click( function () {
+  console.log(btnIngresa)
 })
 
+function ingresarProductos(){
+    var html = ''
+
+    for(let i = 0; i < stock.length; i++){
+        html += '<tr>';
+        html += '<th scope="row">'+ (i+1) +'</th>';
+        html += '<td>'+ stock[i].producto +'</td>';
+        html += '<td>'+ stock[i].id +'</td>';
+        html += '<td>'+ stock[i].Stock +'</td>';
+        html += '<td>'+ stock[i].Precio +'</td>';
+        html += '</tr>';
+    }
+    ingresoProduto.html(html);
+    console.log(html);
+}
+ingresarProductos();
+
+
+
+
+/*
 
 function Productos(nombre, cantidad) {
     this.nombre = nombre;
@@ -30,3 +72,4 @@ function crearStock(nombre, cantidad) {
 
     stockEntrada.push(crearStock);
 }
+*/
