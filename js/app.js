61ca030e19264519921fc349a14f29584a24ebd0
cesar londoño <slac_@hotmail.com>
2021-07-30 00:12:04 -0400

    0004

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
-console.log('hola');
-/*
-const productosEntrar= $('#productos');
-const cantidadProductos = $('#cantidadEntrada');
-const productoVentas = $('#productosVentas');
-const cantidadVenta = $('#cantidadVenta');
-*/
+
+
+const nombreProducto = $('#nombreProducto');
+const rut = $('#rut');
+const fechaVenta = $('#fechaVenta');
+const unidadesVendidas = $('#unidadesVendidas');
+const nombreCliente = $('#nombreCliente');
+
+
 const btnIngresa = $('#agregar');
 // const btnVender = $('#vender');
 
 const ingresoProduto = $('#ingresoProduto');
+const imprimeVenta = $('#imprimeVenta');
+const mostrarUf = $('#mostrarUf');
 
+let ventas = [];
 
 const stock = [
 
@@ -37,8 +42,10 @@ const stock = [
 ];
 
 btnIngresa.click( function () {
-  console.log(btnIngresa)
-})
+  console.log(btnIngresa);
+  crearVenta();
+  
+});
 
 function ingresarProductos(){
     var html = ''
@@ -54,12 +61,99 @@ function ingresarProductos(){
     }
     ingresoProduto.html(html);
     console.log(html);
-}
+};
+
 ingresarProductos();
 
+function Venta() {
+  this.producto = '';
+  this.cantidad = 0;
+  this.cliente = '';
+  
+}
 
+//funcion que agrega una nueva venta
+function crearVenta() {
+  //variable que instancia una Venta
+  let venta = new Venta();
+  //se recogen los valores ingresados en el formulario
+  venta.producto = nombreProducto.val();
+  venta.cantidad = parseInt(unidadesVendidas.val());
+  venta.cliente = nombreCliente.val();
+  //venta.total = ;
+  
+  //se llena la lista de productos
+  ventas.push(venta);
+  //se imprime la venta en la tabla
+  console.log(ventas);
+  
+  imprimirVentas()
+/*
+  contarVentas();
 
+  imprimirTotalVentas();
 
+  imprimirValorTotalVentas();
+   
+  limpiaFormulario();
+  */
+}
+
+//funcion que itera los registros contenidos en el arreglo listaVentas y las muestra en la tabla
+function imprimirVentas() {
+  //variable que arma la fila con sus columnas para mostrar en la tabla 
+  let html = "";
+
+  /*iterar sobre el arreglo*/
+  let idVenta = 0;
+  total = 0;
+
+  for (let i = 0; i < ventas.length; i++) {
+      idVenta += 1;
+      html += "<tr> <th>" + idVenta + "</th> <td>" + ventas[i].producto + "</td> <td>" + ventas[i].cantidad + "</td> <td>" + ventas[i].cliente + "</td> <td>" + '$' + "</td> </tr>";
+
+      //sumarVentas(listaVentas[i].precio)     
+  }
+
+  //se envian la fila con sus columnas para mostrar en la tabla 
+  imprimeVenta.html(html);
+
+}
+
+function requetsUF() {
+
+  var req = new XMLHttpRequest();
+
+  req.onreadystatechange = function () {
+      if(req.readyState === 4 && req.status === 200){
+          console.log(req.responseText);
+          var responseJSON = JSON.parse(req.responseText);
+          console.log(responseJSON);
+
+          
+          var res = responseJSON.UFs[0].Valor;//dos datos fecha y valor
+          var mostrarValor = $('#mostrarValor');
+          
+          console.log(res);
+         
+
+          //formateamos el valor de la uf
+          
+          var numero = res.replace('.', '');
+          numero = numero.replace(',', '.');
+
+          console.log(numero);
+
+          mostrarUf.html(numero);
+          
+      }
+  }
+  req.open("GET", "https://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey=0b918912656eeabcfd5312c27e619891238b881b&formato=json")
+
+  req.send();
+}
+
+requetsUF()
 /*
 
 function Productos(nombre, cantidad) {
